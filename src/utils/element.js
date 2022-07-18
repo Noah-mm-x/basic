00c31f6a61f39aa7fd939fafd6db775a172a416f
孟凡旭 <15733225953@163.com>
2022-07-18 18:44:03 +0800

    add: 添加element

---
+++ src/utils/element.js
@@ -0,0 +1,6 @@
+
+import Vue from 'vue'
+import './../assets/css/element-variables.scss'
+
+import { Button } from 'element-ui'
+Vue.use(Button)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+    <el-button type="primary">主要按钮</el-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
