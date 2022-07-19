e2e79f70ca33a2953128e3600ad0593ad0571e29
孟凡旭 <15733225953@163.com>
2022-07-19 16:13:38 +0800

    引入echarts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,23 +5,84 @@
       src="../assets/logo.png"
     >
     <el-button type="primary">主要按钮</el-button>
+    <div
+      id="chart"
+      ref="chart"
+      style="width: 100%;height:400px;"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import * as echarts from 'echarts'
 export default {
   name: 'HomeView',
   components: {
   },
   async mounted () {
-    console.log(11, this.$post)
-    const params = {
-      a: 1
+    // console.log(11, this.$post)
+    // const params = {
+    //   a: 1
+    // }
+    // const data = await this.$post('baid', params)
+    // console.log(data)
+    this.$nextTick(() => {
+      this.init()
+    })
+  },
+  methods: {
+    init () {
+      // 若是echarts只想在当前页面引用，可以直接引入，然后修改即可！
+      const myChart = echarts.init(document.getElementById('chart'))
+      // 以下全局，我引入echarts属性（若是多个页面需要引入echarts的话，一定要在main.js中引入
+      // const myChart = this.$echarts.init(this.$refs.chart);
+      // 指定图表的配置项和数据
+      const option = {
+        tooltip: {
+          trigger: 'item'
+        },
+        legend: {
+          top: '5%',
+          left: 'center'
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            itemStyle: {
+              borderRadius: 10,
+              borderColor: '#fff',
+              borderWidth: 2
+            },
+            label: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: '40',
+                fontWeight: 'bold'
+              }
+            },
+            labelLine: {
+              show: false
+            },
+            data: [
+              { value: 1048, name: '搜索引擎' },
+              { value: 735, name: '直接访问' },
+              { value: 580, name: '邮件营销' },
+              { value: 484, name: '联盟广告' },
+              { value: 300, name: '视频广告' }
+            ]
+          }
+        ]
+        // 使用刚指定的配置项和数据显示图表。
+      }
+      myChart.setOption(option)
     }
-    const data = await this.$post('baid', params)
-    console.log(data)
   }
 }
 </script>
